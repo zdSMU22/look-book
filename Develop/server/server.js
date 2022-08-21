75a2f251d9f31f5dffb6a635f2713b6d07b9e26a
Dixon <zdixon91.zd@gmail.com>
2022-08-21 09:58:01 -0500

    'setting up server

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require ("apollo-server-express");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +18,12 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
+const startApolloServer = async () => {
+  await server.start();
+}
+
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+startApolloServer();
