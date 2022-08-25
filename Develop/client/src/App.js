40d79aa2f06f6e89d3cd8e028d7c4d158a0362bd
zdSMU22 <94766160+zdSMU22@users.noreply.github.com>
2022-08-24 21:17:50 -0500

    Update App.js

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -3,6 +3,27 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { ApolloServer};
+import { createHttpLink };
+
+const graphql = createHttpLink ({
+  uri: '/graphql',
+});
+
+const jwtLink = ((_, { Headers}) => {
+  const idToken = localStorage.getItem('id_token');
+
+  return{
+    header: {
+      authorization: idToken,},
+    }
+  }
+
+);
+const client = new ApolloServer ({
+  link: authlink.concat(createHttpLink),
+  cache: new InMemoryCache(),
+})
 
 function App() {
   return (
@@ -20,3 +41,4 @@ function App() {
 }
 
 export default App;
+
